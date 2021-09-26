13a203c55859461d2c230e6ae408531c2dca68cc
Kshitiz Pandey <pandey.kshitiz05@gmail.com>
2021-09-26 22:21:47 +0530

    Layouts accessed in activity

--- app/src/main/java/com/kshtiz/getFunny/MainActivity.kt
+++ app/src/main/java/com/kshtiz/getFunny/MainActivity.kt
@@ -2,10 +2,16 @@ package com.kshtiz.getFunny
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvMessage = findViewById<TextView>(R.id.textView)
+        val bGenerateMessage = findViewById<Button>(R.id.button)
+        val bSendMessage = findViewById<Button>(R.id.button2)
     }
 }
